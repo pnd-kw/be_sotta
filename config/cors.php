3a215d393b64af749a1cd784e7495d9e59802ebc
Pandu Office PC <pandu.usahaku@gmail.com>
2025-09-24 15:11:01 +0700

    modify: add localhost origins in cors

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://sottasouvenir.com', 'http://localhost:3000', 'https://sotta-a7lp7uav4-krisna-pandu-wibowos-projects.vercel.app', 'https://sotta.vercel.app', 'https://sottaart.com', 'https://www.sottaart.com'],
+    'allowed_origins' => ['https://sottasouvenir.com', 'http://localhost:3000', 'localhost:3000', 'http://127.0.0.1:3000', 'https://sotta-a7lp7uav4-krisna-pandu-wibowos-projects.vercel.app', 'https://sotta.vercel.app', 'https://sottaart.com', 'https://www.sottaart.com'],
 
     'allowed_origins_pattern' => [],
 
